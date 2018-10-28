d766d0bb816e197f769c1ca2fdcae1a8962f978e
Rachel Wey <rachelwey96@outlook.com>
2018-10-28 05:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   console.log("Welcome, " + name + ". You are number "+ katzDeliLine.length + " in line.")
+  return("Welcome, " + name + ". You are number "+ katzDeliLine.length + " in line.")
 }
 
 function nowServing(katzDeliLine){
