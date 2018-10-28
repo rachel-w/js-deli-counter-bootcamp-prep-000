360e97980fa3b912c8dcb313457d4834c49e8c8a
Rachel Wey <rachelwey96@outlook.com>
2018-10-28 05:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
     console.log("There is nobody waiting to be served!")
   } else{
-    return ("Currently serving "+ katzDeliLine.shift())
+    return ("Currently serving "+ katzDeliLine.shift()+".")
   }
 }
 
