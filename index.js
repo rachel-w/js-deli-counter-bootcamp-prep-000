1530d87c0273fd9cb4b06d45bd6477a3cb40d1c8
Rachel Wey <rachelwey96@outlook.com>
2018-10-28 05:07:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(currentLine, name){
+  currentline.push(name)
   var position = currentLine.length + 1
   console.log("Welcome, ${name}. You are number ${position} in line.")
 }
