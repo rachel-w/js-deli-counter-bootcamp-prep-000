81c2f05f9b82e2bb4e0a1c680ab4b51035810232
Rachel Wey <rachelwey96@outlook.com>
2018-10-28 05:19:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
-function takeANumber(katzDeli, name){
+function takeANumber(katzDeliLine, name){
   katzDeli.push(name)
   console.log("Welcome, " + name + ". You are number "+ katzDeliLine.length + " in line.")
 }
